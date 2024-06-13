0403459d2dd3d62dd5c2caa8dbfc1a23d34c748f
GeorgeOpreaC <158497260+GeorgeOpreaC@users.noreply.github.com>
2024-06-13 17:28:20 +0300

    Delete Curs11/Main directory

--- Curs11/Main/ChallengeClasa.java
+++
@@ -1,9 +0,0 @@
-package Curs11.Main;
-
-public class ChallengeClasa {
-    public static void main(String[] args) {
-       Om Camelia= new Om("Camelia",22,5030802,true);
-       Om Dragos= new Om("Dragos",20,5030802,true);
-
-    }
-}
